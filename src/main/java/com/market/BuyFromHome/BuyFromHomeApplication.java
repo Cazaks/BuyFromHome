1369ac1e742aadd80069e79451103e94a208be02
calebezak@gmail.com <calebezak@gmail.com>
2026-04-29 14:54:54 +0100

    Initial Spring Boot backend setup with Docker and AppException calss

---
+++ src/main/java/com/market/BuyFromHome/BuyFromHomeApplication.java
@@ -0,0 +1,14 @@
+package com.market.BuyFromHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyFromHomeApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BuyFromHomeApplication.class, args);
+	}
+
+}

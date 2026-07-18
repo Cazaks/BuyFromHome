6ea8c6b6a333cbdce4551551db388282807f17da
calebezak@gmail.com <calebezak@gmail.com>
2026-07-18 08:16:16 +0100

    Updating the UserLogin dto to reurn proper data to the user

--- src/main/java/com/market/BuyFromHome/dto/requestDto/userRequestDto/UserLoginRequest.java
+++ src/main/java/com/market/BuyFromHome/dto/requestDto/userRequestDto/UserLoginRequest.java
@@ -2,9 +2,11 @@ package com.market.BuyFromHome.dto.requestDto.userRequestDto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+@AllArgsConstructor
 @Getter
 @Setter
 public class UserLoginRequest {

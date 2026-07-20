6a8705fad0b4ba64f8d78ab42861d9f37ba1c6c8
calebezak@gmail.com <calebezak@gmail.com>
2026-07-21 00:13:11 +0100

    MeasurementUnit enums

---
+++ src/main/java/com/market/BuyFromHome/enums/MeasurementUnit.java
@@ -0,0 +1,30 @@
+package com.market.BuyFromHome.enums;
+
+public enum MeasurementUnit {
+
+    DERICA,
+
+    CUSTARD,
+
+    PAINT_BUCKET,
+
+    BAG,
+
+    BASKET,
+
+    KG,
+
+    GRAM,
+
+    LITRE,
+
+    MILLILITRE,
+
+    PIECE,
+
+    DOZEN,
+
+    CRATE,
+
+    PACK
+}

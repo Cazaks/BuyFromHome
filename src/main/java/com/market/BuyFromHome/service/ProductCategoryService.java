07a77064b2f609c863764bbcccd0d61412c828cb
calebezak@gmail.com <calebezak@gmail.com>
2026-07-21 14:58:57 +0100

    Updating ProductCategoryService interface

--- src/main/java/com/market/BuyFromHome/service/ProductCategoryService.java
+++ src/main/java/com/market/BuyFromHome/service/ProductCategoryService.java
@@ -1,4 +1,31 @@
 package com.market.BuyFromHome.service;
 
+import com.market.BuyFromHome.dto.requestDto.productCategoryRequest.ProductCategoryRequestDto;
+import com.market.BuyFromHome.dto.responseDto.productCategoryResponse.ProductCategoryResponseDto;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
 public interface ProductCategoryService {
+    @Transactional
+    ProductCategoryResponseDto createProductCategory(
+            ProductCategoryRequestDto requestDto);
+
+    @Transactional
+    ProductCategoryResponseDto getProductCategoryById(Long id);
+
+    @Transactional
+    List<ProductCategoryResponseDto> getAllProductCategories();
+
+
+    @Transactional
+    ProductCategoryResponseDto updateProductCategory(
+            Long id,
+            ProductCategoryRequestDto requestDto);
+
+    @Transactional
+    ProductCategoryResponseDto disableProductCategory(Long id);
+
+    @Transactional
+    ProductCategoryResponseDto enableProductCategory(Long id);
 }

60fc57a6f0d0bc679407d035836b5a3be130d94c
calebezak@gmail.com <calebezak@gmail.com>
2026-07-24 09:48:50 +0100

    Creating ProductRepository and ProductService interface

---
+++ src/main/java/com/market/BuyFromHome/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.market.BuyFromHome.repository;
+
+import com.market.BuyFromHome.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    boolean existsByProductNameIgnoreCase(String productName);
+}
---
+++ src/main/java/com/market/BuyFromHome/service/ProductService.java
@@ -0,0 +1,10 @@
+package com.market.BuyFromHome.service;
+
+import com.market.BuyFromHome.dto.requestDto.productRequest.ProductRequestDto;
+import com.market.BuyFromHome.dto.responseDto.productResponse.ProductResponseDto;
+import jakarta.transaction.Transactional;
+
+public interface ProductService {
+    @Transactional
+    ProductResponseDto createProduct(ProductRequestDto requestDto);
+}

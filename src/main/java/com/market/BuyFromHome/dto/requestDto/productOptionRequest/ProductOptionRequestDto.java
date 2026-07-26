174c2bde3d5faf59ad975edb3a16a070c4214c73
calebezak@gmail.com <calebezak@gmail.com>
2026-07-26 10:01:11 +0100

    Modifying the request and response dtos to match my design specification

--- src/main/java/com/market/BuyFromHome/dto/requestDto/productOptionRequest/ProductOptionRequestDto.java
+++ src/main/java/com/market/BuyFromHome/dto/requestDto/productOptionRequest/ProductOptionRequestDto.java
@@ -1,14 +1,9 @@
 package com.market.BuyFromHome.dto.requestDto.productOptionRequest;
 
-import com.market.BuyFromHome.enums.MeasurementUnit;
-import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.math.BigDecimal;
 
 @Getter
 @Setter
@@ -22,15 +17,4 @@ public class ProductOptionRequestDto {
 
     @Size(max = 100, message = "Product specification cannot exceed 100 characters.")
     private String productSpecification;
-
-    @NotNull(message = "Measurement unit is required.")
-    private MeasurementUnit measurementUnit;
-
-    @NotNull(message = "Product price is required.")
-    @DecimalMin(value = "0.01", message = "Product price must be greater than zero.")
-    private BigDecimal productPrice;
-
-    @NotNull(message = "Product quantity is required.")
-    @Positive(message = "Product quantity must be greater than zero.")
-    private Integer productQuantityInStock;
 }
--- src/main/java/com/market/BuyFromHome/dto/responseDto/productOptionResponse/ProductOptionResponseDto.java
+++ src/main/java/com/market/BuyFromHome/dto/responseDto/productOptionResponse/ProductOptionResponseDto.java
@@ -1,18 +1,15 @@
 package com.market.BuyFromHome.dto.responseDto.productOptionResponse;
 
-import com.market.BuyFromHome.enums.MeasurementUnit;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Builder
 public class ProductOptionResponseDto {
-
 
     private Long productOptionId;
 
@@ -23,12 +20,6 @@ public class ProductOptionResponseDto {
     private String productVariety;
 
     private String productSpecification;
-
-    private MeasurementUnit measurementUnit;
-
-    private BigDecimal productPrice;
-
-    private Integer productQuantityInStock;
 
     private boolean enabled;
 

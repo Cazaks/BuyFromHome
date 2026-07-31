6b32a9fd61cfff48d352d17a1abfcc1b126dcdda
calebezak@gmail.com <calebezak@gmail.com>
2026-07-31 05:27:54 +0100

    Creating Address repository an AddressService interface

---
+++ src/main/java/com/market/BuyFromHome/service/AddressService.java
@@ -0,0 +1,9 @@
+package com.market.BuyFromHome.service;
+
+import com.market.BuyFromHome.dto.requestDto.addressRequest.AddressRequestDto;
+import com.market.BuyFromHome.dto.responseDto.addressResponse.AddressResponseDto;
+
+public interface AddressService {
+    AddressResponseDto createAddress(
+            Long userId, AddressRequestDto requestDto);
+}
